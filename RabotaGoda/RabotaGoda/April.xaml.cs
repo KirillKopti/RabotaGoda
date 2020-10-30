edbb5f69db339d34b196d3d5e83162293526beb5
Kirill <Kiril@PROMETHEUS>
2020-10-30 09:59:36 +0200

    qwe

---
+++ RabotaGoda/RabotaGoda/April.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RabotaGoda
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class April : TabbedPage
+    {
+        public April()
+        {
+            InitializeComponent();
+        }
+    }
+}
