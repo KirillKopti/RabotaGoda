af2445cfbc2c2d497a4e800cbdc41a29266e8292
Kirill <Kiril@PROMETHEUS>
2020-11-01 17:55:01 +0200

    434343434

---
+++ RabotaGoda/RabotaGoda/September.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RabotaGoda
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class September : TabbedPage
+    {
+        public September()
+        {
+            InitializeComponent();
+        }
+    }
+}
